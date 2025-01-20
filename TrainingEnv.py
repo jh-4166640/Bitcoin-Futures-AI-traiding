3a61e5e009d3d739553c8eb3fa3d83c01c676fe8
ptolemaeus <75351831+jh-4166640@users.noreply.github.com>
2025-01-20 15:12:22 +0900

    Update 25-01-20

--- TrainingEnv.py
+++ TrainingEnv.py
@@ -3,6 +3,7 @@ import pandas as pd
 import random
 import sys
 
+
 class TrainingEnv():
     def __init__(self, data, inital_balance=100, fee_rate=0.001, position_rate = 0.4):
         # data road
@@ -18,36 +19,57 @@ class TrainingEnv():
         self.entry_price = 0    
         
         self.alpha = 0.05 # learning rate
-        self.gamma = 0.4 # 할인율
-        self.epsilon = 0.1 # 탐험율 exploration rate
+        self.gamma = 0.97 # 할인율
+        self.epsilon = 0.9 # 탐험율 exploration rate
         
         # Q-tabel init
         self.q_table = {}
         self.actions = [-2,-1,0,1,2]
+        self.trades = []
+        self.net_worth_Log=[]
+
+    def choose_action(self, state):
+        if state not in self.q_table:
+            self.q_table[state] = np.zeros(len(self.actions))
+        # 점진적으로 epsilon 감소
+        self.epsilon = max(0.01, self.epsilon * 0.995)  # 최소 0.01까지 감소
+        if random.uniform(0, 1) < self.epsilon:
+            return random.choice(self.actions)  # 탐험
+        else:
+            return np.argmax(self.q_table[state])  # 최대 Q-value 선택
+    def choose_action2(self, state):        
+        if state not in self.q_table:
+            self.q_table[state] = {a: {'q_value': 0, 'profit': 0} for a in self.actions} 
+            self.epsilon = max(0.01, self.epsilon * 0.996) 
+            if random.uniform(0, 1) < self.epsilon: 
+                return random.choice(self.actions) 
+            else: 
+                return max(self.q_table[state], key=lambda x: self.q_table[state][x]['q_value'])
+
+    def resetEnv(self):
+        self.current_step = 0
+        self.balance = self.initial_balance
+        self.position = 0
+        self.net_worth = self.initial_balance
+        self.entry_price = 0
+        self.trades = []
+        self.net_worth_Log=[]
+
         
     def setHyperParam(self,alpha, gamma, epsilon):
         self.alpha = alpha
         self.gamma = gamma
         self.epsilon = epsilon
         self.current_step = 0
-        print(f"alpha {self.alpha},,, gamma {self.gamma},,, epsilon {self.epsilon}")
+        #print(f"alpha {self.alpha},,, gamma {self.gamma},,, epsilon {self.epsilon}")
     def getHyperParam(self):
         return self.alpha, self.gamma, self.epsilon
     def get_q_value(self, state, action):  
         if state not in self.q_table:
             self.q_table[state] = np.zeros(len(self.actions))  # Q-table 초기화
         return self.q_table[state][action]
-    
-    def choose_action(self, state):
-         # 상태가 Q-table에 없으면 초기화
-        if state not in self.q_table:
-            self.q_table[state] = np.zeros(len(self.actions))
-        # 탐험(ε-greedy) 방식으로 행동 선택
-        if random.uniform(0,1) < self.epsilon:
-            return random.choice(self.actions) # 무작위 선택
-        else :
-            q_values = self.q_table[state]
-            return np.argmax(q_values) # Q-value가 최대인 행동 선택
+    def get_q_table(self):
+        return self.q_table
         
     def update_q_value(self, state, action, reward, next_state):
         # Q-value 업데이트
@@ -56,6 +78,8 @@ class TrainingEnv():
         # Q-learning 업데이트 공식
         new_q = current_q + self.alpha * (reward + self.gamma * max_future_q - current_q)
         self.q_table[state][action] = new_q
+        #self.q_table[state][action]['q_value'] = new_q
+        #self.q_table[state][action]['profit'] = reward
         
     def Enter_long(self,current_price):
         if self.position == 0:
@@ -65,7 +89,8 @@ class TrainingEnv():
             if self.balance > trade_cost:
                 # 현재 자산 - (현재 자산*포지션 진입 비율) - 거래수수료
                 self.position = (self.balance*self.position_rate)/current_price
-                self.balance -= self.balance*self.position_rate - trade_cost            
+                self.balance -= self.balance*self.position_rate - trade_cost
+                self.trades.append((self.current_step, current_price ,'LONG'))            
                 
     def Exit_long(self, current_price):
         if self.position > 0:
@@ -74,6 +99,7 @@ class TrainingEnv():
             trade_cost = trade_value * self.fee_rate
             self.balance += (trade_value - trade_cost)
             self.position = 0
+            self.trades.append((self.current_step, current_price,'EXIT LONG'))
             
     def Enter_short(self, current_price):
         if self.position == 0:
@@ -85,6 +111,7 @@ class TrainingEnv():
                 self.position = -(self.balance*self.position_rate)/current_price
                 self.balance -= self.balance*self.position_rate - trade_cost
                 self.entry_price = current_price
+                self.trades.append((self.current_step, current_price,'SHORT'))
                 
     def Exit_short(self, current_price):
         if self.position < 0:
@@ -94,12 +121,13 @@ class TrainingEnv():
             self.balance += (trade_value - trade_cost)
             self.position = 0
             self.entry_price = 0
+            self.trades.append((self.current_step, current_price,'EXIT SHORT'))
             
     def step(self):
         # now price information
         current_price = self.data.iloc[self.current_step]["Close"]
         state = (current_price, self.position) # 상태 정의
-        action = trade.choose_action(state)
+        action = self.choose_action(state)
         reward = 0
         # 추가 포지션 진입 없음
         # 롱 포지션 진입
@@ -122,7 +150,7 @@ class TrainingEnv():
             
         # 자산 업데이트
         self.net_worth = self.balance + profit
-        
+        self.net_worth_Log.append(self.net_worth)
         # 보상 (자산) 증/감
         reward = self.net_worth - self.initial_balance
         #if abs(action) == 2:
@@ -132,7 +160,9 @@ class TrainingEnv():
         next_state = (next_price, self.position)
         self.update_q_value(state, action,reward, next_state)
         return reward
-        
+    
+
+    
 if __name__ == "__main__":
     try: 
         csv_data = pd.read_csv('BTCUSDT20250115.csv')
@@ -148,27 +178,25 @@ if __name__ == "__main__":
     except Exception as e:
         print(f"Exception :  {e}")
         sys.exit(1)
-    max_total = 0
+    max_total = -999999999
     epoch = 0
     print("Stored data")
     trade = TrainingEnv(csv_data)
     while True:
         print(f"epoch({epoch})")
-        trade.setHyperParam(random.uniform(0,0.5),random.uniform(0.89,0.99),random.uniform(0,0.5))
-        
-        if epoch > 1000:
+        trade.resetEnv()
+        if epoch > 100:
             break
         total_reward  = 0
-        trade = TrainingEnv(csv_data, 100)
-        for episode in range(318):
-            for step in range(5000):
-                reward = trade.step()
-                total_reward += reward
-                #print(f"Episode: {episode + 1}, Step: {step + 1}, Total Reward: {total_reward}")
-        #print(f"Episode {episode + 1} finished with total reward: {total_reward}")
-        if max_total > total_reward:
-            max_total = total_reward
-            max_alpha, max_gamma, max_epsilon = trade.getHyperParam()
-            print(f"max total : {max_total},,, max alpha : {max_alpha},,, max gamma : {max_gamma},,, max epsilon : {max_epsilon}")
+        #trade.setHyperParam(random.uniform(0,0.8),random.uniform(0.89,0.99),random.uniform(0,0.9))
+        for episode in range(1590000): # 795*2000 , 395*2000
+            reward = trade.step()
+            total_reward += reward
+        print(f"Episode {episode + 1} finished with total reward: {total_reward}")  
+        trades_df = pd.DataFrame(trade.trades, columns=['Step', 'Price','Action'])
+        net_worth_df = pd.DataFrame(trade.net_worth_Log, columns=['Net worth'])
+        combined_df = trades_df.copy()
+        combined_df['Net worth'] = net_worth_df
+        combined_df.to_csv(f'combined_log{epoch}.csv', index=False)
         total_reward = 0
         epoch+=1
